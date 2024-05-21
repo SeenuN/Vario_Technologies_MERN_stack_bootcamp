3cf997634f0999d5cd3b7c11bedfd33353a5b553
SeenuN <seenun469@gmail.com>
2024-05-21 11:41:06 +0530

    task7

---
+++ task7/src/script.js
@@ -0,0 +1,23 @@
+// script.js
+$(document).ready(function() {
+    // Task 1: Display Welcome Message
+    $('#welcomeButton').click(function() {
+        $('#welcomeMessage').show();
+    });
+
+    // Task 2: Change background color of input boxes on click
+    $('.inputBox').click(function() {
+        $(this).css('background-color', 'yellow');
+    });
+
+    // Task 3: Toggle styles on element
+    $('#toggleStyleButton').click(function() {
+        $('#styleElement').toggleClass('style1 style2');
+    });
+
+    // Task 4: Display text box content on button
+    $('#textBoxButton').click(function() {
+        var text = $('#textBox').val();
+        $(this).text(text);
+    });
+});
